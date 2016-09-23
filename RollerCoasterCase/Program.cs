9b222fb5d67db1721e867cb288c266f091340ed5
Kurt Weber <weberkrt@msn.com>
2016-09-22 22:17:49 -0400

    First commit

---
+++ RollerCoasterCase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using 
+
+namespace RollerCoasterCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("..\\..\\..\\RollerCoaster.txt");
+
+            using (reader)
+            {
+                // Count to determine number of lines
+                int lineNumber = 0;
+
+                // Read first line
+                string line = reader.ReadLine();
+
+                // Read the other lines in file
+                while (line != null)
+                {
+                    // lineNumber = lineNumber + 1
+                    lineNumber++;
+
+                    // Changing single letter
+                    string replaceLetter = 
+                    
+                    // Write to console
+                    Console.Write(replaceLetter);
+
+                    // Read next line
+                    line = reader.ReadLine();
+                    Console.WriteLine();
+
+                }
+            }
+        }
+    }
+}
